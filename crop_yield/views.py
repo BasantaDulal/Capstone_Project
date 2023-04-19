3690c96cced437a60d5029f0d98c909c65cd7d34
Basanta Dulal <109762085+BasantaDulal@users.noreply.github.com>
2023-04-19 10:13:59 -0400

    final commit

--- crop_yield/views.py
+++ crop_yield/views.py
@@ -1,8 +1,40 @@
-# Importing the necessary library
 from django.shortcuts import render
 
+# Define the yield per hectare for each crop
+CROP_YIELDS = {
+    'Apple': 19860,
+    'Beans': 1891,
+    'Grapes': 1330,
+    'Maize': 9263,
+    'Peas': 2291,
+    'Rice': 3000,
+    'Soybeans': 3039,
+}
 
-# A view function that returns the 'crop_yield_prediction.html' template
+
 def crop_yield_prediction(request):
-    # Render the template with the context and return an HTTPResponse
-    return render(request, 'crop_yield_prediction.html')
+    if request.method == 'POST':
+        # Get the user's inputs from the form
+        crop = request.POST.get('crop')
+        area = float(request.POST.get('area'))
+
+        # Calculate the predicted yield based on the crop and area
+        if crop in CROP_YIELDS:
+            yield_per_hectare = CROP_YIELDS[crop]
+            predicted_yield = yield_per_hectare * area
+        else:
+            predicted_yield = None
+
+        # Render the same page with the results included
+        return render(request, 'crop_yield_prediction.html', {
+            'crops': CROP_YIELDS.keys(),
+            'selected_crop': crop,
+            'selected_area': area,
+            'yield_prediction': predicted_yield,
+        })
+    else:
+        # Render the initial form page
+        return render(request, 'crop_yield_prediction.html', {
+            'crops': CROP_YIELDS.keys(),
+        })
+
